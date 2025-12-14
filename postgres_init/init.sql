d7ee5ce385c3b1cfb9721b3ec2eeec54def10d2e
Jakub Kłoda <kubakloda82@gmail.com>
2025-12-14 20:14:16 +0100

    Update model_registry and model_usage_metrics tables

--- postgres_init/init.sql
+++ postgres_init/init.sql
@@ -1,30 +1,32 @@
 -- 1. TABELA REJESTRU 
--- Przechowuje aktualny stan i metadane modelu.
 CREATE TABLE IF NOT EXISTS model_registry (
     id SERIAL PRIMARY KEY,
-    model_name VARCHAR(100) UNIQUE NOT NULL, -- np. 'digit-recognizer-v1'
-    health_status VARCHAR(20) DEFAULT 'OK',  -- Status: 'OK', 'ERROR', 'LOADING'
+    model_name VARCHAR(100) UNIQUE NOT NULL,
+    health_status VARCHAR(20) DEFAULT 'OK',
     last_called_at TIMESTAMP,                
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- 2. TABELA METRYK
--- Przechowuje historię obciążenia w oknach czasowych.
+-- 2. TABELA METRYK (Zaktualizowana o koniec okna)
 CREATE TABLE IF NOT EXISTS model_usage_metrics (
     model_name VARCHAR(100) NOT NULL,
-    time_window TIMESTAMP NOT NULL,       -- Początek okna, np. 14:00, 14:15
-    request_count INTEGER DEFAULT 0,      -- Licznik requestów w tym oknie
     
-    -- Klucz złożony: Jeden wpis na model w danym oknie czasowym
-    PRIMARY KEY (model_name, time_window),
+    time_window_start TIMESTAMP NOT NULL,  -- Początek okna
+    time_window_end TIMESTAMP NOT NULL,    -- Koniec okna
     
-    CONSTRAINT fk_model_registry --Przed wprowadzeniem danych dla modelu wymagane jest zarejestrownaie go
+    request_count INTEGER DEFAULT 0,
+    
+    -- Klucz złożony: Start okna definiuje unikalność w danym okresie
+    PRIMARY KEY (model_name, time_window_start),
+    
+    CONSTRAINT fk_model_registry 
         FOREIGN KEY(model_name) 
         REFERENCES model_registry(model_name)
         ON DELETE CASCADE
 );
 
+-- SEED DATA
 INSERT INTO model_registry (model_name, health_status, last_called_at) 
 VALUES ('digit-recognizer-v1', 'OK', NOW())
 ON CONFLICT DO NOTHING;
-
 
